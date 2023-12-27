31eecd56674bb9f909950b1f64d2835d81c9f38b
waddah27 <waddahkh.ali94@gmail.com>
2023-12-28 01:41:53 +0300

    optimized the speed of video streaming using webrtc by reducing number of processed frames

--- Exercise52Class.py
+++ Exercise52Class.py
@@ -13,20 +13,28 @@ class Exercise52:
     def __init__(self) -> None:
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
         self.gr25 =Fingers_5_2_exercise()
         self.frame_count_thresh = 5
-        self.reset()
+        self.feedback_text = None
+        self.restart()
+        self.reset_counters()
 
-    def reset(self):
-        self.num_hands = 0
+    def reset_counters(self):
+        self.frame_counter = 0
         self.num_correct = 0
         self.num_wrong = 0
         self.num_raise_hands_fbks = 0
+
+    def restart(self):
+        self.num_hands = 0
         self.gestures = [None, None]
-        self.hands = self.mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+
 
 
     def run_fingers_5_2_exercise(self, frame):
+
+        self.frame_counter+=1
         text_shift = 0
         # BGR 2 RGB
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -38,7 +46,10 @@ class Exercise52:
         image.flags.writeable = False
 
         # Detections
-        results = self.hands.process(image)
+        if self.frame_counter%self.frame_count_thresh ==0:
+            results = self.hands.process(image)
+        else:
+            results = None
 
         # Set flag to true
         image.flags.writeable = True
@@ -47,7 +58,7 @@ class Exercise52:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Rendering results
-        if results.multi_hand_landmarks:
+        if results is not None and results.multi_hand_landmarks:
             self.num_hands = len(results.multi_hand_landmarks)
 
             for num, hand in enumerate(results.multi_hand_landmarks):
@@ -68,37 +79,46 @@ class Exercise52:
                     cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 lmk_arr = get_lmks_array_3D(hand)
 
-        #         gesture, gesture_int = self.gr25.check_5_plus_2_performance(lmk_arr)
-        #         # if gesture_int ==2 or gesture_int==5:
-        #         self.gestures[num] = gesture_int
-        #         cv2.putText(image, gesture, (coord[0],coord[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                gesture, gesture_int = self.gr25.check_5_plus_2_performance(lmk_arr)
 
-        #         # Render Wrist Angles
-        #         text_shift -= 30
-        #         image = vis_wrist_axs(image, hand)
-        # print(self.gestures)
-        # print(self.num_hands)
+                # if gesture_int ==2 or gesture_int==5:
+                self.gestures[num] = gesture_int
+                cv2.putText(image, gesture, (coord[0],coord[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # if self.num_hands < 2:
-        #     self.num_raise_hands_fbks+=1
-        #     self.num_correct = 0
-        #     self.num_wrong = 0
-        #     if self.num_raise_hands_fbks > self.frame_count_thresh:
-        #         # num_raise_hands_fbks = 0
-        #         image = cv2.putText(image, 'Raise both hands and make 5+2', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        # elif self.gestures != [2, 5] and self.gestures != [5, 2]:
-        #     print(f"{self.gestures} != {[2, 5]} or {[5, 2]}")
-        #     self.num_wrong +=1
-        #     self.num_correct = 0
-        #     self.num_raise_hands_fbks = 0
-        #     if self.num_wrong > self.frame_count_thresh:
-        #         # num_wrong = 0
-        #         image = cv2.putText(image, 'wrong, make 5 with one hand and 2 with the other', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        # else:
-        #     self.num_correct+=1
-        #     self.num_wrong = 0
-        #     self.num_raise_hands_fbks = 0
-        #     if self.num_correct > self.frame_count_thresh:
-        #         # num_correct = 0
-        #         image = cv2.putText(image, 'Correct', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                # Render Wrist Angles
+                text_shift -= 30
+                image = vis_wrist_axs(image, hand)
+        print(self.gestures)
+        print(self.num_hands)
+
+        if self.num_hands < 2:
+            self.num_raise_hands_fbks+=1
+            self.num_correct = 0
+            self.num_wrong = 0
+            if self.num_raise_hands_fbks > self.frame_count_thresh:
+                self.feedback_text = 'Raise both hands and make 5+2'
+                # num_raise_hands_fbks = 0
+                image = cv2.putText(image, self.feedback_text , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        elif self.gestures != [2, 5] and self.gestures != [5, 2]:
+            print(f"{self.gestures} != {[2, 5]} or {[5, 2]}")
+            self.num_wrong +=1
+            self.num_correct = 0
+            self.num_raise_hands_fbks = 0
+            if self.num_wrong > self.frame_count_thresh:
+                if -1 in self.gestures and 5 in self.gestures:
+                    self.feedback_text = "Wrong, use index and middle fingers to make 2"
+                elif -1 in self.gestures and 5 not in self.gestures:
+                    self.feedback_text = "use index and middle fingers to make 2, make 5 in the other hand"
+                else:
+                    self.feedback_text = f'wrong, make 5 in one hand and 2 in the other'
+                # num_wrong = 0
+                image = cv2.putText(image, self.feedback_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        else:
+            self.num_correct+=1
+            self.num_wrong = 0
+            self.num_raise_hands_fbks = 0
+            if self.num_correct > self.frame_count_thresh:
+                self.feedback_text = 'Correct'
+                # num_correct = 0
+                image = cv2.putText(image, self.feedback_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         return image
--- Fingers_5_2_recognition.py
+++ Fingers_5_2_recognition.py
@@ -8,38 +8,9 @@ class Fingers_5_2_exercise(GestureRecognition):
     def __init__(self) -> None:
         super().__init__()
         self.thr_angle_thumb = 30.
-        self.thumb_tolerance_factor = 0.5
+        self.thumb_tolerance_factor = 0.1
 
-    # def _finger_status(self, lmkArr: np.ndarray) -> List[bool]:
-    #     """Detect each finger if open
 
-    #     args:
-    #         lmlist (ndarray): array of predicted hand landmarks 21x2
-    #     Returns:
-    #         List[bool]: list of [True if finger is open, False other wise]
-    #     """
-    #     ### TODO: rewrite this code in the correct way
-    #     fingerList = []
-    #     finger_tips = [lmkArr[4], lmkArr[8], lmkArr[12], lmkArr[16], lmkArr[20]]
-    #     finger_thresh = [lmkArr[6], lmkArr[7], lmkArr[11], lmkArr[15], lmkArr[19]]
-    #     thumb_tolerance = lmkArr[5][1] - self.thumb_tolerance_factor * (lmkArr[5][1]-lmkArr[6][1])
-    #     thumb_angle = self._vector_2_angle(
-    #         np.array([lmkArr[0][0] - lmkArr[2][0], lmkArr[0][1] - lmkArr[2][1]]),
-    #         np.array([lmkArr[3][0] - lmkArr[4][0], lmkArr[3][1] - lmkArr[4][1]])
-    #     )
-    #     for i, (ftip, ftresh) in enumerate(zip(finger_tips, finger_thresh)):
-    #         if i==0:
-    #             if ftip[1] < ftresh[1] or thumb_angle < self.thr_angle_thumb:
-    #                 fingerList.append(True)
-    #             else:
-    #                 fingerList.append(False)
-    #         else:
-    #             if ftip[1] < ftresh[1]:
-    #                 fingerList.append(True)
-    #             else:
-    #                 fingerList.append(False)
-
-    #     return fingerList
     def _finger_status(self, lmkArr: np.ndarray) -> List[bool]:
         """Detect each finger if open
 
@@ -74,7 +45,7 @@ class Fingers_5_2_exercise(GestureRecognition):
             if firstOpen and secondOpen:
                 return 'Two',number
             else:
-                return 'Raise index and middle fingers to make number two', number
+                return 'Raise index and middle fingers to make number two', -1
         else:
             return f'{number}, Raise 5 or 2 fingers (index and middle)', number
 
